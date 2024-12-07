1decdd96726bdfa6c9f06e83c860c5ced1d270d1
Hallous Yassine <yasshallous@gmail.com>
2024-12-07 20:33:42 +0100

    Update my_data.h

--- Embedded/src/my_data.h
+++ Embedded/src/my_data.h
@@ -1,8 +1,8 @@
 #ifndef MY_DATA
 #define MY_DATA
 
-#define SSID "Galaxy A14 2869"
-#define PASS "kzt9qeau6yxtbnx"
-#define SERVER_IP "http://192.168.172.15"
+#define SSID "ADMIN"
+#define PASS "1234"
+#define SERVER_IP "http://Adresse_IP"
 
 #endif
